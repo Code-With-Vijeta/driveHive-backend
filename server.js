f3e7de2a9deba041045225a2853ee7543db804ea
Code-With-Vijeta <codevijeta@gmail.com>
2025-09-29 21:16:14 +0530

    feat: Integrated context loading check and cleaned up Home page

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ await connectDB();
 app.use(cors({
   origin: [process.env.CLIENT_URL, "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+  credentials: true,
 }));
 
 
